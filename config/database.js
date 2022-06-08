12f1852ef0055f141d6158cd30a3ddfe7327552f
whatcoteam <whatcoteam@gmail.com>
2022-06-08 13:11:42 +0430

    buildspec and db update

--- config/database.js
+++ config/database.js
@@ -1,40 +1,31 @@
-// const path = require('path');
+const path = require('path');
 
 
-// if(process.env.NODE_ENV == 'production') {
-module.exports = ({ env }) => {
-  console.log({
-    host: env("DATABASE_HOST", "127.0.0.1"),
-    port: env.int("DATABASE_PORT", 5432),
-    database: env("DATABASE_NAME", "postgres"),
-    user: env("DATABASE_USERNAME", ""),
-    password: env("DATABASE_PASSWORD", ""),
-  })
-  return {
-    connection: {
-      client: 'postgres',
+if(process.env.NODE_ENV == 'production') {
+  module.exports = ({ env }) => ({
       connection: {
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "postgres"),
-        user: env("DATABASE_USERNAME", ""),
-        password: env("DATABASE_PASSWORD", ""),
+        client: 'postgres',
+        connection: {
+          host: env("DATABASE_HOST", "127.0.0.1"),
+          port: env.int("DATABASE_PORT", 5432),
+          database: env("DATABASE_NAME", "postgres"),
+          user: env("DATABASE_USERNAME", ""),
+          password: env("DATABASE_PASSWORD", ""),
+        },
+        ssl: false,
       },
-      ssl: false,
+    })
+} else {
+  module.exports = ({ env }) => ({
+    connection: {
+      client: 'sqlite',
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
     },
-  }
-};
-// } else {
-//   module.exports = ({ env }) => ({
-//     connection: {
-//       client: 'sqlite',
-//       connection: {
-//         filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-//       },
-//       useNullAsDefault: true,
-//     },
-//   });
-// } 
+  });
+} 
 
 
 
